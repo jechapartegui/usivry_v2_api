c38b43ca56d5cd2b7de4f64da48f4b1e229ddbba
J-E <jechapartegui@gmail.com>
2023-09-05 16:54:24 +0200

    0509-2

--- logout.php
+++ logout.php
@@ -1,17 +1,8 @@
 <?php
-
-session_start();
+include_once ("config/session.php");
 // We need to keep the federation in the session so we will set it again after removing everything!!!
 
 session_unset();
 
-if (isset($_COOKIE['login_key'])){
-	// Remove the login cookie on explicit logout!
-	setcookie('login_key','',1);
-}
-
-if (isset($_COOKIE['language'])){
-	setcookie('language','',1);
-}
 
 print json_encode(array('result'=>'OK','username'=>''));
